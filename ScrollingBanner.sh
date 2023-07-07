be8c93609060858062c2fd864323413136c0a6b3
Caleb <110494428+griswoct@users.noreply.github.com>
2023-07-07 08:47:33 -0500

    Restructured Strategy and Added If Statements
    Restructured code to better align with goal. Added if statements.

--- ScrollingBanner.sh
+++ ScrollingBanner.sh
@@ -13,38 +13,66 @@ FillChar='#'	#Possible options: 8, @, #, $, &, B, E, W, M, H, X
 Spacing=2	#Number of spaces between the letters
 PadSize=$(($Size+$Spacing))
 BannerText='Example'	#Text to display
-n=$(($WinWidth/$PadSize))	#Number of whole letters in each display line
+Remainder=''	#For filling in partial letter at the end
 i=0	#Loop counter
 j=0	#Loop counter
 k=0	#Loop counter
+n=0	#Letter number index
+p=0	#Partial letter index
 
 read -p "What text do you want to display" $BannerText	#Get text to display
 
 $i=0
-while [ $i -lt $Size ]
+while [ $i -lt $Size ]	#Populate banner line by line
 do
 	$j=0
-	while [ $j -lt $n ]	#Get the $i line of the first n letters
+	$n=0
+	while [ $j -lt $WinWidth ]	#Populate display line i accross window
 	do
-		#Add line $i of letter $BannerText[$j] to OutputLine$i
-		$k=0
-		while [ $k -lt $Spacing ]	#Add whitespace between letters
-		do
-			#Add ' ' to OutputLine$i
-			(($k++))
-		done
-		(($j++))
+		if [ $(($WinWidth - $j)) -ge $PadSize ]
+		then
+			#Add line $i of letter $BannerText[$n] to OutputLine$i
+			$j=$(($j+$Size))
+			$k=0
+			while [ $k -lt $Spacing ]	#Add whitespace between letters
+			do
+				#Add ' ' to OutputLine$i
+				(($k++))
+				(($j++))
+			done
+			(($n++))
+		else
+			#Get $i line of letter $bannerText[$n] as $Remainder$i
+			while [ $k -lt $Spacing ]	#Add whitespace between letters
+			do
+				#Add ' ' to $Remainder$i
+				(($k++))
+			done
+			$p=0
+			while [ $j -lt $WinWidth ]
+			do
+				#Add $Remainder[$p] to the end of $OutputLine$i
+				(($p+))
+				(($j++))
+			done
+		fi
 	done
-	#Add partial letter n+1 to OutputLine$i
 	#Echo OutputLine$i '/n'
 	(($i++))
 done
+
 #Return to begining of the banner
 $i=0
-while [ $i -lt $Size ]
+while [ $i -lt $Size ]	#Increment banner to the left line by line
 do
 	#Remove first character of OutputLine$i
-	#Add next character or letter n+1
+	if [ $p -gt $PadSize ]
+	then
+		#load in the next letter
+		#Create new $Remainder$i
+	else
+	#Add $Remainder[$p] to $OutputLine$i
 	#Echo OutputLine$i '/n'
 	(($i++))
 done
+(($p++))

df510cd9b6a8dae0ec89baaf10c0113e12e77716
Caleb <110494428+griswoct@users.noreply.github.com>
2023-07-14 10:32:21 -0500

    Fixed whats_my_line Function
    Moved the whats_my_line function before the main body of the script and fixed syntax errors. Moved the spacing loop inside of whats_my_line

--- ScrollingBanner.sh
+++ ScrollingBanner.sh
@@ -5,7 +5,7 @@
 #PURPOSE: DISPLAY SCROLLING BANNER IN THE TERMINAL
 #LICENSE: THE UNLICENSE
 #AUTHOR: CALEB GRISWOLD
-#UPDATED: 2023-07-13
+#UPDATED: 2023-07-14
 #
 # Ideads: infinite scroll back to back, infinite scroll clear between
 #
@@ -14,16 +14,190 @@ Length=0	#Number of letters in banner message
 Size=5	#High and width of banner letter in characters
 i=0	#Banner display line
 j=0	#Loop counter
-k=0	#Loop counter
 n=0	#Working banner letter index
 p=0	#Partial letter character index
 Spacing=2	#Number of spaces between banner letters
 PadSize=$(($Size+$Spacing))
 C1='#'	#Fill character, possible options: #, @, $, &, 8, B, E, W, M, H, X
-C0='.'	#Empty character, possible options: ' ', _, -, .
+C0='.'	#Empty character, possible options: _, -, .
 BannerText='Example Text'	#Text to display
 Remainder=()	#Working banner end letter
 Output=()	#Output strings to display banner
+
+whats_my_line () {
+	local list=()	#Boolean array defining banner display letter matrix
+	local h=0	#Height of display letter
+	local w=0	#Width of display letter
+	local a=0	#Offset
+	local b=0	#Local loop counter
+	case ${BannerText:$n:1} in	#Find correct letter
+		' ')
+			h=5
+			w=5
+			list=(false false false false false false false false false false false false false false false false false false false false false false false false false)	#Display a 5x5 space
+			;;
+		A | a)
+			h=5
+			w=5
+			list=(false true true true false true false false false true true true true true true true false false false true true false false false true)
+			;;
+		B | b)
+			h=5
+			w=5
+			list=(true true true true false true false false false true true true true true false true false false false true true true true true false)
+			;;
+		C | c)
+			h=5
+			w=5
+			list=(false true true true true true false false false false true false false false false true false false false false false true true true true)
+			;;
+		D | d)
+			h=5
+			w=5
+			list=(true true true true false true false false true true true false false false true true false false true true true true true true false)
+			;;
+		E | e)
+			h=5
+			w=5
+			list=(true true true true true true false false false false true true true false false true false false false false true true true true true)
+			;;
+		F | f)
+			h=5
+			w=5
+			list=(true true true true true true false false false false true true true false false true false false false false true false false false false)
+			;;
+		G | g)
+			h=5
+			w=5
+			list=(false true true true false true false false false false true false true true true true false false false true false true true true false)
+			;;
+		H | h)
+			h=5
+			w=5
+			list=(true false false false true true false false false true true true true true true true false false false true true false false false true)
+			;;
+		I | i)
+			h=5
+			w=5
+			list=(false true true true false false false true false false false false true false false false false true false false false true true true false)
+			;;
+		J | j)
+			h=5
+			w=5
+			list=(false false true true true false false false true false false false false true false true false false true false false true true false false)
+			;;
+		K | k)
+			h=5
+			w=5
+			list=(true false false false true true false false true false true true true false false true false false true false true false false false true)
+			;;
+		L | l)
+			h=5
+			w=5
+			list=(true false false false false true false false false false true false false false false true false false false false true true true true false)
+			;;
+		M | m)
+			h=5
+			w=5
+			list=(true false false false true true true false true true true false true false true true false true false true true false false false true)
+			;;
+		N | n)
+			h=5
+			w=5
+			list=(true false false false true true true false false true true false true false true true false false true true true false false false true)
+			;;
+		O | o)
+			h=5
+			w=5
+			list=(false true true true false true false false false true true false false false true true false false false true false true true true false)
+			;;
+		P | p)
+			h=5
+			w=5
+			list=(true true true true false true false false false true true true true true false true false false false false true false false false false)
+			;;
+		Q | q)
+			h=5
+			w=5
+			list=(false true true true false true false false false true true false false false true true false false true false false true true false true)
+			;;
+		R | r)
+			h=5
+			w=5
+			list=(true true true true false true false false false true true true true true false true false false true false true false false false true)
+			;;
+		S | s)
+			h=5
+			w=5
+			list=(false true true true true true false false false false false true true true false false false false false true true true true true false)
+			;;
+		T | t)
+			h=5
+			w=5
+			list=(true true true true true false false true false false false false true false false false false true false false false false true false false)
+			;;
+		U | u)
+			h=5
+			w=5
+			list=(true false false false true true false false false true true false false false true true false false false true false true true true false)
+			;;
+		V | v)
+			h=5
+			w=5
+			list=(true false false false true true false false false true true false false false true false true false true false false false true false false)
+			;;
+		W | w)
+			h=5
+			w=5
+			list=(true false false false true true false false false true true false true false true true true false true true true false false false true)
+			;;
+		Z | x)
+			h=5
+			w=5
+			list=(true false false false true false true false true false false false true false false false true false true false true false false false true)
+			;;
+		Y | y)
+			h=5
+			w=5
+			list=(true false false false true true false false false true false true true true false false false true false false false false true false false)
+			;;
+		Z | z)
+			h=5
+			w=5
+			list=(true true true true true false false false true false false false true false false false true false false false true true true true true)
+			;;
+		*)	#Character not recognized, display block
+			h=5
+			w=5
+			list=(true true true true true true true true true true true true true true true true true true true true true true true true true )	#Display a 5x5 block
+			;;
+	esac
+	if [ $Size -ne $h -o $Size -ne $w ]
+	then
+		echo 'SIZE MISMATCH ERROR'
+		exit
+	fi
+	local a=$(($w * $i))
+	local b=0
+	local str=''
+	while [ $b -lt $w ]	#Get line $i of banner letter
+	do
+		if [ ${list[$(($a+$b))]} == true ]
+		then
+			str+=$C1
+		else
+			str+=$C0
+		fi
+		((b++))
+	done
+	b=0
+		while [ $b -lt $Spacing ]	#Add whitespace to Remainder
+		do
+			str+=$C0
+			((b++))
+		done
+	Remainder[$i]=$str
+}
 
 read -p $'What text do you want to display?\n' BannerText	#Get text to display
 Length=${#BannerText}
@@ -40,23 +214,10 @@ do
 			whats_my_line	#Get line $i of letter $BannerText[$n]
 			Output[$i]+=${Remainder[$i]}
 			j=$(($j+$Size))
-			k=0
-			while [ $k -lt $Spacing ]	#Add whitespace between letters
-			do
-				OutputLine[$i]+=$C0
-				((k++))
-				((j++))
-			done
 			((n++))
 		else
 			Remainder[$i]=''
 			whats_my_line	#Get $i line of letter $bannerText[$n]
-			k=0
-			while [ $k -lt $Spacing ]	#Add whitespace to Remainder
-			do
-				Remainder[$i]+=$C0
-				((k++))
-			done
 			p=0
 			while [ $j -lt $Width ]	#Add characters from ramaining banner letter until full display width
 			do
@@ -85,156 +246,12 @@ do
 			fi
 			Remainder[$i]=''
 			whats_my_line	#Get $i line of letter $BannerText[$n]
-			while [ $k -lt $Spacing ]	#Add whitespace to Remainder
-			do
-				Remainder[$i]+=' '
-				((k++))
-			done
 		fi
 		Output[$i]=${Output[$i]:1}	#Remove first character of $Output[$i]
 		Output[$i]+=${Remainder[$i]:$p:1}	#Add next character the end of $Output[$i]
-		echo ${Output[$i]} #$'\n'
+		echo $Output[$i]
 		((i++))
 	done
 	((p++))
 	sleep 0.2
 done
-
-whats_my_line () {
-	local list=()
-	local h=5
-	local w=5
-	case $BannerText[$n] in	#Find correct letter
-		' ')
-			$h=5
-			$w=5
-			$list=(false false false false false false false false false false false false false false false false false false false false false false false false false)	#Display a 5x5 space
-		A | a)
-			$h=5
-			$w=5
-			$list=(false true true true false true false false false true true true true true true true false false false true true false false false true)
-		B | b)
-			$h=5
-			$w=5
-			$list=(true true true true false true false false false true true true true true false true false false false true true true true true false)
-		C | c)
-			$h=5
-			$w=5
-			$list=(false true true true true true false false false false true false false false false true false false false false false true true true true)
-		D | d)
-			$h=5
-			$w=5
-			$list=(true true true true false true false false true true true false false false true true false false true true true true true true false)
-		E | e)
-			$h=5
-			$w=5
-			$list=(true true true true true true false false false false true true true false false true false false false false true true true true true)
-		F | f)
-			$h=5
-			$w=5
-			$list=(true true true true true true false false false false true true true false false true false false false false true false false false false)
-		G | g)
-			$h=5
-			$w=5
-			$list=(false true true true false true false false false false true false true true true true false false false true false true true true false)
-		H | h)
-			$h=5
-			$w=5
-			$list=(true false false false true true false false false true true true true true true true false false false true true false false false true)
-		I | i)
-			$h=5
-			$w=5
-			$list=(false true true true false false false true false false false false true false false false false true false false false true true true false)
-		J | j)
-			$h=5
-			$w=5
-			$list=(false false true true true false false false true false false false false true false true false false true false false true true false false)
-		K | k)
-			$h=5
-			$w=5
-			$list=(true false false false true true false false true false true true true false false true false false true false true false false false true)
-		L | l)
-			$h=5
-			$w=5
-			$list=(true false false false false true false false false false true false false false false true false false false false true true true true false)
-		M | m)
-			$h=5
-			$w=5
-			$list=(true false false false true true true false true true true false true false true true false true false true true false false false true)
-		N | n)
-			$h=0
-			$w=0
-			$list=(true false false false true true true false false true true false true false true true false false true true true false false false true)
-		O | o)
-			$h=0
-			$w=0
-			$list=(false true true true false true false false false true true false false false true true false false false true false true true true false)
-		P | p)
-			$h=0
-			$w=0
-			$list=(true true true true false true false false false true true true true true false true false false false false true false false false false)
-		Q | q)
-			$h=0
-			$w=0
-			$list=(false true true true false true false false false true true false false false true true false false true false false true true false true)
-		R | r)
-			$h=0
-			$w=0
-			$list=(true true true true false true false false false true true true true true false true false false true false true false false false true)
-		S | s)
-			$h=0
-			$w=0
-			$list=(false true true true true true false false false false false true true true false false false false false true true true true true false)
-		T | t)
-			$h=0
-			$w=0
-			$list=(true true true true true false false true false false false false true false false false false true false false false false true false false)
-		U | u)
-			$h=0
-			$w=0
-			$list=(true false false false true true false false false true true false false false true true false false false true false true true true false)
-		V | v)
-			$h=0
-			$w=0
-			$list=(true false false false true true false false false true true false false false true false true false true false false false true false false)
-		W | w)
-			$h=0
-			$w=0
-			$list=(true false false false true true false false false true true false true false true true true false true true true false false false true)
-		Z | x)
-			$h=0
-			$w=0
-			$list=(true false false false true false true false true false false false true false false false true false true false true false false false true)
-		Y | y)
-			$h=0
-			$w=0
-			$list=(true false false false true true false false false true false true true true false false false true false false false false true false false)
-		Z | z)
-			$h=0
-			$w=0
-			$list=(true true true true true false false false true false false false true false false false true false false false true true true true true)
-		*)	#Character not recognized, display block
-			$h=5
-			$w=5
-			$list=(true true true true true true true true true true true true true true true true true true true true true true true true true )	#Display a 5x5 block
-	esac
-	if [ $Size -ne $h ]
-	then
-		echo 'SIZE MISMATCH ERROR'
-		exit
-	fi
-	local a=$(($w * $i))
-	local b=0
-	local str=''
-	while [ $b -lt $Size ]	#Get line $i of banner letter
-	do
-		if [ $list[$(($a + $b)) ]
-		then
-			$str+=$C1
-		else
-			$str+=$C0
-		fi
-		(($b++))
-	done
-	return $str
-}

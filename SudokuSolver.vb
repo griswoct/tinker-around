53ce224013bf8b38ef91afbbf390aee8f58d5289
Caleb <110494428+griswoct@users.noreply.github.com>
2023-08-11 11:24:48 -0500

    Added TwoPair Subroutine
    Look for exclusive pairs of pairs and eliminate other options.

--- SudokuSolver.vb
+++ SudokuSolver.vb
@@ -3,53 +3,53 @@
 'PURPOSE: SOLVE A SUDOKU PUZZLE IN EXCEL WITH A MACRO
 'LICENSE: THE UNLICENSE
 'AUTHOR: CALEB GRISWOLD
-'UPDATED: 2023-08-04
+'UPDATED: 2023-08-11
 '
 'This program is an exercise in better understanding of VBA for Excel.
 'NOTE:  INITIAL SUDOKU PUZZLE MUST BE PLACED IN RANGE: G2:O10
 '       SOLUTION WILL APPEAR IN RANGE G12:O20
-'IDEAS: If only two options remain, and they are in the same column, row, or section
-'       Then eliminate thoes two options elseware in the column, row, or section
-'       And remove other options from thoese two cells
-'       Mark rows, columns, subsections, or numbers as done and skip checking them in the future
+'IDEAS: Mark rows, columns, subsections, or numbers as done and skip checking them in the future
+		'Check of the working matrix has changed between itterations, stop solving if no progress
 
 Option Explicit
 
 Sub Solve()
-    Dim blanks, z, max As Integer
-    Call Clear
-    Call Initialize
-    blanks = 81     '81 squares in a traditional Sudoku grid
-    max = 9     'Max number of itterations
-    z = 0
-    Do While blanks > 0
-        Call SimpleElimination      'Remove n as an option if it is already in the solution for that row, column, or subsection
-        Call OnlyOption     'Scan each cell and fill in the solution if only one option remains
-        Call SimpleElimination      'Not needed if clean up is added to OnlyOption
-        Call aPlaceForEverything        'If only one location possible for n, fill in the solution
-        blanks = NumBlanks
-        Range("Q19").Value = blanks
-        If z > max Then     'Abort after max itterations
-            GoTo MaxItterations
-        End If
-        z = z + 1
-        Range("Q20").Value = z      'Record number of itterations
-    Loop
-    If NumBlanks = 0 Then
-        MsgBox "Solved!"
-    End If
-    Exit Sub
+	Dim blanks, z, max As Integer
+	Call Clear
+	Call Initialize
+	blanks = 81     '81 squares in a traditional Sudoku grid
+	max = 9     'Max number of itterations
+	z = 0
+	Do While blanks > 0
+		Call SimpleElimination      'Remove n as an option if it is already in the solution for that row, column, or subsection
+		Call OnlyOption     'Scan each cell and fill in the solution if only one option remains
+		Call SimpleElimination      'Not needed if clean up is added to OnlyOption
+		Call aPlaceForEverything        'If only one location possible for n, fill in the solution
+		Call TicTacToe      'If the only locations for n in a submatrix are in the same row/column, remove n option elsewhere in the row/column
+		Call OnlyOption     'Scan each cell and fill in the solution if only one option remains
+		Call TwoPair    'Finds pairs of possible solutions in common pairs of cells, and eliminates all other options
+		blanks = NumBlanks
+		Range("Q19").Value = blanks
+		If z > max Then     'Abort after max itterations
+			GoTo MaxItterations
+		End If
+		z = z + 1
+		Range("Q20").Value = z      'Record number of itterations
+	Loop
+	If NumBlanks = 0 Then
+		MsgBox "Solved!"
+	End If
+	Exit Sub
 MaxItterations:
-    MsgBox "Maximum number of itterations reached:" & max + 1
+	MsgBox "Maximum number of itterations reached:" & max + 1
 End Sub
 
 Sub Clear()     'Delete all values from the solution and working matricies, and reset formating.
-    Range("G12:O20").ClearContents      'Solution matrix
-    Range("G12:O20").Font.Bold = False
+    Range("G12:O20").Font.Bold = False	'Solution matrix
     Range("G12:O20").Font.Italic = False
     Range("G12:G20").Font.Color = RGB(0, 0, 0)
-    Range("G22:O102").ClearContents     'Working matrix
-    Range("G22:O102").Font.Bold = False
+	Range("G12:O20").ClearContents      'Solution matrix
+    Range("G22:O102").Font.Bold = False	'Working matrix
     Range("G22:O102").Font.Italic = False
     Range("G22:O102").Select
         With Selection.Interior
@@ -57,6 +57,7 @@ Sub Clear()     'Delete all values from the solution and working matricies, and 
         .TintAndShade = 0
         .PatternTintAndShade = 0
         End With
+	Range("G22:O102").ClearContents		'Working matrix
     Range("Q19").Value = 0      'Number of blanks in solution
     Range("Q20").Value = 0      'Number of itterations
 End Sub
@@ -72,7 +73,6 @@ Sub Initialize()        'Copy initial values to the solution matrix, and populat
                 Range("F13").Offset(9 * i, j).Cells(n, 1).Value = n  'Copy initial value to working matrix
                 Range("F13").Offset(9 * i, j).Cells(n, 1).Font.Bold = True
                 Range("F13").Offset(9 * i, j).Select
-                'ActiveCell.Range("A1:A9").Interior.ColorIndex = 5
             Else        'Populate working matrix options
                 For n = 1 To 9
                     Range("F13").Offset(9 * i, j).Cells(n, 1).Value = n
@@ -249,23 +249,19 @@ Sub TicTacToe()     'If 1 to 3 possible locations remain for n in a subsection, 
                 If x = 1 Then   'Only 1 spot n could be located in subsection , and solution cell is blank
                     Range("G12:O20").Cells(g + k1 + 1, h + l1) = n   'Fill in solution
                     Range("G12:O20").Cells(g + k1 + 1, h + l1).Font.Italic = True    'Italic solution
-                    Range("G12:O20").Cells(g + k1 + 1, h + l1).Font.Color = RGB(0, 255, 0)   'For testing
                     For i = 1 To 9      'Delete other options from working matrix for this cell
                         If i <> n Then
                             Range("G22:O102").Cells(9 * g + 9 * k1 + i, h + l1).Value = ""
-                            'Range("G22:O102").Cells(9 * g + 9 * k1 + i, h + l1).Interior.Color = RGB(0, 255, 0) 'For testing
                         End If
                     Next i
                     For j = 1 To 9  'Delete n as an option elsewhere in the row g + k1
                         If j <> h + l1 Then      'Ignore solution cell
                             Range("G22:O102").Cells(9 * g + 9 * k1 + n, j).Value = ""
-                            'Range("G22:O102").Cells(9 * g + 9 * k1 + n, j).Interior.Color = RGB(0, 255, 0)  'For testing
                         End If
                     Next j
                     For i = 0 To 8  'Delete n as an option elsewhere in the column h + l1
                         If i <> g + k1 Then      'Ignore solution cell
                             Range("G22:O102").Cells(9 * i + n, h + l1).Value = ""
-                            'Range("G22:O102").Cells(9 * i + n, h + l1).Interior.Color = RGB(0, 255, 0)  'For testing
                         End If
                     Next i
                 ElseIf x = 2 Or x = 3 Then
@@ -276,20 +272,16 @@ Sub TicTacToe()     'If 1 to 3 possible locations remain for n in a subsection, 
                     If k1 = k2 And k2 = k3 Then 'All possible locations for n within the subsection are in the same row
                         For j = 1 To 9  'Delete n as an option elsewhere in the row g + k
                             If j = h + l1 Or j = h + l2 Or j = h + l3 Then      'Possible solution locations
-                                'Range("G22:O102").Cells(9 * g + 9 * k1 + n, j).Font.Color = RGB(255, 0, 0)   'Possible value in red for testing
                             Else
                                 Range("G22:O102").Cells(9 * g + 9 * k1 + n, j).Value = ""
-                                'Range("G22:O102").Cells(9 * g + 9 * k1 + n, j).Interior.Color = RGB(255, 0, 0)  'For testing
                             End If
                         Next j
                     End If
                     If l1 = l2 And l2 = l3 Then 'All possible locations for n within the subsection are in the same column
                         For i = 0 To 8  'Delete n as an option elsewhere in the column h + l
                             If i = g + k1 Or i = g + k2 Or i = k3 Then      'Possible solution locations
-                                'Range("G22:O102").Cells(9 * i + n, h + l1).Font.Color = RGB(255, 0, 0)  'Possible value in red for testing
                             Else
                                 Range("G22:O102").Cells(9 * i + n, h + l1).Value = ""
-                                'Range("G22:O102").Cells(9 * i + n, h + l1).Interior.Color = RGB(255, 0, 0)  'For testing
                             End If
                         Next i
                     End If
@@ -300,6 +292,61 @@ NextSection:
             g = g + 3
         Loop
     Next n
+End Sub
+
+Sub TwoPair()
+	Dim g, h, i, j, k, l, m, n As Integer
+	Dim aCol1(8), aCol2(8), aRow1(8), aRow2(8) As Integer
+	g = 0
+	Do While g < 9  'Itterate through subsection (rows)
+		h = 0
+		Do While h < 9  'Itterate through subsection (columns)
+			For n = 1 To 9	'Itterate through possible numbers
+				k = 0   'Number of spots n could be in subsection (g, h)
+				For i = 0 To 2  'Itterate through subrows
+					For j = 1 To 3  'Itterate through subcolumns
+						If Range("G12:O20").Cells(g + i + 1, h + j) = n Then  'Solution for n already found in subsection
+							aCol2(n - 1) = 0
+							GoTo Nextn
+						ElseIf Range("G22:O102").Cells(9 * g + 9 * i + n, h + j) <> "" Then   'If possible value listed in working matrix
+							If k = 0 Then	'First possible location for n
+								aRow1(n - 1) = i
+								aCol1(n - 1) = j
+							ElseIf k = 1 Then	'Two possible locations for n
+								aRow2(n - 1) = i
+								aCol2(n - 1) = j	'aCol2 <> 0 indicates a pair exists
+							ElseIf k = 2 Then	'Three or more possible locations for n
+								aCol2(n - 1) = 0
+								GoTo Nextn
+							End If
+							k = k + 1
+						End If
+					Next j
+				Next i
+				Nextn:
+			Next n
+			For l = 1 To 8
+				If aCol2(l - 1) = 0 Then	'm is not a pair
+					GoTo Nextl
+				End If
+				For m = l + 1 To 9	'Don't check reversed pairs (e.g. 5 vs 7 is the same as 7 vs 5)
+					If aCol2(m - 1) = 0 Then	'm is not a pair
+						'm is not a pair
+					ElseIf aRow1(l - 1) = aRow1(m - 1) And aCol1(l - 1) = aCol1(m - 1) And aRow2(l - 1) = aRow2(m - 1) And aCol2(l - 1) = aCol2(m - 1) Then	'found a pair of pairs
+						For k = 1 To 9	'Itterate through options for this cell
+							If k <> l And k <> m Then	'Skip the two possible solutions
+								Range("G22:O102").Cells(9 * g + 9 * aRow1(l - 1) + k, h + aCol1(l - 1)).Value = ""
+								Range("G22:O102").Cells(9 * g + 9 * aRow2(l - 1) + k, h + aCol2(l - 1)).Value = ""
+							End If
+						Next k
+					End If
+				Next m
+				Nextl:
+			Next l
+			h = h + 3
+		Loop
+		g = g + 3
+	Loop
 End Sub
 
 Function NumBlanks() As Integer
@@ -340,4 +387,3 @@ ErrorHandler:
     MsgBox "Duplicate values found in the same row or column!"
     NumBlanks = 99       'There 81 squares in a Sudoku puzzle
 End Function
-

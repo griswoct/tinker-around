c01a9635e5f80320dfe3cebf0c6311d53fc2bc13
Caleb <110494428+griswoct@users.noreply.github.com>
2023-08-19 22:57:28 -0500

    Blocked out smallestUnused()
    Added function to find the smallest integer not already used

--- YellowstonePermutation.js
+++ YellowstonePermutation.js
@@ -10,17 +10,17 @@
 //Use the smallest valid number
 //New term must be relatively prime to the previous term
 //New term must have a common factor with the term before last
-//
-//array n = 1, 2, 3		//Solution array
-//array u = 1, 2, 3		//Already used numbers
-//array pf1 = 1		//Prime factorization 1
-//array pf2 = 1		//Prime factorization 2
-//int min = 4		//Smallest unused integer
-//int x = 4		//Guess for next term
-//int i = 3		//Array index
-//
-//i = (size of n) - 1
-//min = smallestUnused(0)
+
+//const n = [1, 2, 3];		//Solution array
+//const u = [1, 2, 3];		//Already used numbers
+//const pf1 = [1];		//Prime factorization 1
+//const pf2 = [1	];	//Prime factorization 2
+//let min = 4;		//Smallest unused integer
+//let x = 4;		//Guess for next term
+//let i = 3;	//Array index
+
+//i = n.length;
+//min = smallestUnused(1)
 //x = min
 //Loop start
 	//pf1 = primeFactors(n[i-1])
@@ -30,21 +30,40 @@
 		//pf2 = primeFactors(x)
 	//Loop While End
 	//pf1 = primeFactor(n[i-2])
-	//If pf1 and pf2 share a common term:	//x has a common term with the term before last
+	//If pf1 and pf2 share a common term {	//x has a common term with the term before last
 		//n[i] = x
-		//add2U(x)		//added x to the array u, in sorted order
-		//If x = min:		//Smallest available integer was the next term
+		//add2u(x)		//added x to the array u, in sorted order
+		//If (x = min)	 {	//Smallest available integer was the next term
 			//min = smallestUnused(min)		//New smallest unused integer
-		//End If
 		//x = min
 		//i++
-	//Else:
+	//} else {
 		//x = smallestUnused(x)
-	//End If
-//Loop end
+	//}
+//}
 
 //Functions:
 	//Prime factorization, save result?
 	//Smallest unused integer (larger than invalid option)
 	//Add a integer to the array u
 	//Shared terms between pf1 and pf2
+
+function smallestUnused(m){ //finds the smallest unused integer greater than m
+	//let j = m - 1;
+//if (u[j] = m) { //all integers less than m have been used, start at m
+//} else { //there are missing integers before m, work backwards to m
+//loop while u[j] > m and u[j] != j+1 {
+//j--;
+//}
+//}
+//let l = u.length;
+//let y = u[j];
+//while j < l {
+//if (u[j+1] = y + 1) {
+//j++;
+//y++;
+//} else {
+//y++, exit loop
+//}
+//retrun y
+}

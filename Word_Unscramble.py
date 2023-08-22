62d771f39bf1f4d6567904defbf8e5ae3c8b7a7f
Caleb <110494428+griswoct@users.noreply.github.com>
2023-08-21 23:06:40 -0500

    Create Word_Unscramble.py
    Ideas for solving word

---
+++ Word_Unscramble.py
@@ -0,0 +1,12 @@
+#scramble words to provide material
+#need common english words, likely nouns
+#Word Attributes:
+  #Length
+  #First letter
+  #Type (noun, verb, adjective, adverb, etc.)
+  #Common or uncommon
+  #Repeated letters
+  #Subject (common: animals, seasonal, wedding, baby, sports, religion, geography, literature, food)
+#Sub dictionaries?
+#Object or relationsl database?
+#Order by popularity, try nouns first

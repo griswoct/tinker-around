f91a7593023036ee2a7a8efba861d93dfe0a0b27
Caleb <110494428+griswoct@users.noreply.github.com>
2023-08-25 11:34:06 -0500

    Dictionary Build Plan

--- Word_Unscramble.py
+++ Word_Unscramble.py
@@ -11,12 +11,25 @@
 #Object or relationsl database?
 #Order by popularity, try nouns first
 
-#Dictionary database (json?)
+#Dictionary database (csv, json, or pickle):  #csv and json are portable, pickle is native
   #Word
   #Sorted word (letters arranged alphabetically)
   #Length
   #Popularity
   #Part of Speech
+
+#Dictionary build:
+  #Create *.csv file in Excel as a starting point
+  #Import with pandas as working dictionary
+  #Read nouns.md
+  #For each noun:
+    #Find in working dictionary
+    #Assign part-of-speech (PoS) = noun
+    #If noun[i] not in working dictionary
+      #Add noun at end of working dictionary
+  #For first n number of words without PoS:
+    #Prompt user input for PoS
+    #OR API call to dictionary?
 
 #Solver:
   #Sort letters alphabetically

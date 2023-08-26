f77c9bfc85498361b59f2131c513a817bcf64099
Caleb <probigbro@gmail.com>
2023-08-26 10:00:01 -0500

    Added add2u and commonFactor Functions

--- YellowstonePermutation.js
+++ YellowstonePermutation.js
@@ -3,7 +3,7 @@
 //PURPOSE: Output a series of numbers following the yellowstone purmutation
 //LICENSE: THE UNLICENSE
 //AUTHOR: CALEB GRISWOLD
-//UPDATED: 2023-08-15
+//UPDATED: 2023-08-26
 //
 //YELLOWSTONE PERMUTATION:
 //No terms are repeated
@@ -11,29 +11,30 @@
 //New term must be relatively prime to the previous term
 //New term must have a common factor with the term before last
 
-const n = [1, 2, 3];		//Solution array
-const u = [1, 2, 3];		//Already used numbers
-const pf1 = [1];		//Prime factorization 1
-const pf2 = [1	];	//Prime factorization 2
-let min = 4;		//Smallest unused integer
-let x = 4;		//Guess for next term
+const n = [1, 2, 3];	//Solution array
+const u = [1, 2, 3];	//Already used numbers
+const pf1 = [1];	//Prime factorization 1
+const pf2 = [1];	//Prime factorization 2
+const numTerms = 10;	//number of terms to calculate
+let min = 4;	//Smallest unused integer
+let x = 4;	//Guess for next term
 let i = 3;	//Array index
 
 i = n.length;
 min = smallestUnused(1)
 x = min
-loop {
+for (let h = 0; h < numTerms; h++) {
 	pf1 = primeFactors(n[i-1])
 	pf2 = primeFactors(x)
-	while (/*pf1 and pf2 share a common term*/) {	//Find an unused integer that does not have a common factor with the previous term
+	while (commonFactor() == true) {	//Find an unused integer that does not have a common factor with the previous term
 		x = smallestUnused(x)
 		pf2 = primeFactors(x)
 	}
 	pf1 = primeFactor(n[i-2])
-	If (/*pf1 and pf2 share a common term*/) {	//x has a common term with the term before last
+	If (commonFactor() == true) {	//x has a common term with the term before last
 		n[i] = x
 		add2u(x)		//added x to the array u, in sorted order
-		If (x = min)	{	//Smallest available integer was the next term
+		If (x == min)	{	//Smallest available integer was the next term
 			min = smallestUnused(min)		//New smallest unused integer
 		x = min
 		i++
@@ -43,16 +44,9 @@ loop {
 	}
 }
 
-//Functions:
-	//Prime factorization, save result?
-	//Smallest unused integer (larger than invalid option)
-	//Add a integer to the array u
-	//Shared terms between pf1 and pf2
-
 function smallestUnused(m){ //finds the smallest unused integer greater than m
 	let j = m - 1;
-	if (u[j] = m) { 	//all integers less than m have been used, start at m
-	} else {	//there are missing integers before m, work backwards to m
+	if (u[j] != m) {	//there are missing integers before m, work backwards to m
 		while (u[j] > m) {
 			if (u[j] != j + 1) {	//previous integers still missing
 				j--;	//work backwards
@@ -63,8 +57,8 @@ function smallestUnused(m){ //finds the smallest unused integer greater than m
 	}
 	let l = u.length;
 	let y = u[j];
-	while (j < l) {
-		if (u[j+1] = y + 1) {	//all intergers through j+1 used
+	while (j < l) {	//index j must stay within array u
+		if (u[j+1] == y + 1) {	//all intergers through j+1 used
 			j++;
 			y++;
 		} else {	//found unused integer
@@ -73,4 +67,30 @@ function smallestUnused(m){ //finds the smallest unused integer greater than m
 		}
 	}
 	retrun y;	//smallest unused integer greater than m
+}
+
+function primeFactors(p) {	//Find the prime factors of p
+	//PLACEHOLDER
+}
+
+function add2u(m) {	//Adds n to the list of used integers (array u)
+	u.push(m);	//add m to array u
+	u.sort(function(a, b){return a - b});	//sort array u by numberic value
+}
+
+function commonFactor() {	//return true if two arrays share a common term
+	let j = 0;
+	let k = 0;
+	while ((j < pf1.length) && (k < pf2.length)) {	//array indexes must be within the lengths of the arrays
+		if (pf1[j] == pf2[k]) {	//found a common factor
+			return true;
+		} else {	//adjust j or k
+			if (pf1[j] < pf2[k]) {	//pf1 and pf2 must but in numerical order
+				j++;
+			} else {
+				k++;
+			}
+		}
+	}
+	return false;
 }

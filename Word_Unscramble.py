2e81d5e44bd98325fd5997fb10a1d0d036f00be5
Caleb <110494428+griswoct@users.noreply.github.com>
2023-10-24 14:40:58 -0500

    Check for and Create dictionary.csv
    Create dictionary.csv for Word_Unscrable.py if one doesn't already exist.

--- Word_Unscramble.py
+++ Word_Unscramble.py
@@ -32,11 +32,11 @@
   #Save dictionary in csv form with pandas
 
 import pandas
-#if dictionary.csv exists:
-  #read dixtionary.csv as df
-#else:
-df = pandas.read_csv("words.csv")
-#save dictionary.cav
+import os.path
+if os.path.exists('dictionary.csv') == True:
+	df = pandas.read_csv('dictionary.csv')
+else:
+	df = pandas.read_csv('words.csv')
 i = 0
 sort = []
 while i < 9884:
@@ -45,6 +45,8 @@ while i < 9884:
 df['Sort'] = sort
 #df['Sort'] = ''.join(sorted(str(df.Word)))
 #print('Sorted word ', n, ' is ', df.loc[n].at['Sort'])  #for testing
+if os.path.exists('dictionary.csv') == False:
+	df.to_csv('dictionary.csv', sep=',')
 
 #Solver:
   #Sort letters alphabetically
@@ -58,18 +60,21 @@ df['Sort'] = sort
     #Prompt user for answer
     #Add to dictionary
 
-letters = input("Please enter the scrambled letters:")
+letters = input('Please enter the scrambled letters: ')
 letters = ''.join(sorted(letters))
-print("Sorted letters: ", letters)
+#print("Sorted letters: ", letters)	#for testing
 match = False
 j = 0
 while j < 9884:
-  if letters == sort[j]:
-    print('Found match: ', df.loc[j].at['Word'])
-    match = True
-  j += 1
+	if letters == sort[j]:
+		print('Found match: ', df.loc[j].at['Word'])
+		match = True
+	j += 1
 if match == False:
-  print('No match found')
-  prompt = input('Add to dictionary? (Y/N)')
-  if prompt == 'Y' or 'yes' or 'YES' or 'Yes':
-    #Add to dictionary
+	print('No match found')
+	prompt = input('Add to dictionary? (Y/N)')
+	if prompt == 'Y' or 'yes' or 'YES' or 'Yes':
+		word = input('What word do the letters', letters, 'make? >')
+    		df.loc[len(df)] = [len(df)+1, word, letters]	#Add line to dataframe
+		df.to_csv('dictionary.csv', sep=',')
+print('Exiting Word_Unscramble.py')

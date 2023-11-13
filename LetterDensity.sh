48ad939d65fe98182d93b21ce07d68a5aa405009
Caleb <110494428+griswoct@users.noreply.github.com>
2023-11-12 22:27:28 -0600

    Outline for LetterDensity.sh

---
+++ LetterDensity.sh
@@ -0,0 +1,4 @@
+#grab pixal letters
+#go through the list of true/false and add 1 for each true
+#output list of letters and number 0-25 (for 5x5 letters)
+#use these values for first attempt at ASCII Art generator

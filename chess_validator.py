8396c1e6a43bbad1c06efce555cfdd84fefacb40
Caleb <110494428+griswoct@users.noreply.github.com>
2024-01-25 22:58:22 -0600

    Add chess_validator.py Outline

---
+++ chess_validator.py
@@ -0,0 +1,7 @@
+#get board arrangement or start fresh
+#accepts algebraic notation to move pieces
+#validates of a piece can:
+    #move in that way
+    #no piece is in the way
+    #not capturing your own piece or opponents King
+    #dosen't put your King in check

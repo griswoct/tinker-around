ebcaa1a2fb523b3ccae4fea2135433ca87a94ccc
Caleb <110494428+griswoct@users.noreply.github.com>
2024-01-29 17:45:09 -0600

    Add starting position to Chess Validator

--- chess_validator.py
+++ chess_validator.py
@@ -1,7 +1,14 @@
-#get board arrangement or start fresh
+#get board arrangement or start with fresh starting position
 #accepts algebraic notation to move pieces
 #validates of a piece can:
     #move in that way
     #no piece is in the way
     #not capturing your own piece or opponents King
     #dosen't put your King in check
+#Additional ideas:
+    #Heat map of which squares on the boad are controlled by which player
+    #How many attackers a d defenders are there on each piece
+
+start_pos = input("Enter D for default starting position, or FEN notation")
+if start_pos = 'D':
+    start_pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"

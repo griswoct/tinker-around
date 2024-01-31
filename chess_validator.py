2b63955f23b40cb71889c728c87d33067e908726
Caleb <110494428+griswoct@users.noreply.github.com>
2024-01-31 13:21:36 -0600

    Populate Board in Chess Validator
    Populate the board array from the FEN configuration string in chess_validator.py

--- chess_validator.py
+++ chess_validator.py
@@ -7,13 +7,37 @@
     #dosen't put your King in check
 #Additional ideas:
     #Heat map of which squares on the boad are controlled by which player
-    #How many attackers a d defenders are there on each piece
+    #How many attackers and defenders are there on each piece
+    #Identify checkmate
 
 StartPos = input("Enter D for default starting position, or board configuration in FEN notation")
-if StartPos = 'D':
+if fen = 'D':
     StartPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-#define 64 character array board
-#loop through StartPos and decipher FEN notation into board array
+i = 0    #Position in StartPos array
+j = 0    #Board position
+pieces = [K,k,Q,q,R,r,N,n,B,b,P,p]    #Valid pieces list
+board = []
+while j < 64:    #Populate board array from FEN string
+    if fen[i] == '/':    #Next rank
+        i += 1    #Ignore and go to to next character
+        if i mod 8 != 0:    # '/' character encountered midway through a rank
+            print("Unexpected / in the board configuration")
+            break
+    elif fen[i].isdigit() == True:
+        if fen[i] > 8:    #More than 8 empty squares in a rank isn't posible in normal chess
+            print("Unexpected extra empty squares in the board configuration")
+            break
+        else:
+            k = 0
+            while k < fen[i]:
+                board[j] = ' '
+                k += 1
+                j += 1
+    elif fen[i] in pieces:
+        board[j] = fen[i]
+        i += 1
+        j += 1
+#Validate board setup
     #if not 1 king each, throw an error (each color should have exactly 1 King)
     #if not 64 squares accounted for, throw an error (invalid configuration)
     #if more than 8 pawns on either side, throw error (too many pawns)
@@ -33,3 +57,8 @@ move = input("Enter desired move in algebraic notation")
     #if first character is R, N, B, or Q:
         #count number of that piece and color are on the board
 #if count < 1, throuw error (piece does not exist)
+#if count = 1, save start location
+#get last two characters of move, validate that end square is on the board(a1 to h8)
+#if the third to last character of the move is 'x', verify an oponents piece is sitting on that square
+#validate move path against piece type
+#if not a Knight, validate clear path (no pieces in the way)

fbb0e47a2abd5c884e8c01c7ac2684220ea39465
Caleb <110494428+griswoct@users.noreply.github.com>
2024-02-01 09:37:03 -0600

    Chess Validator Cleanup
    Corrected typos
    Added DisplayBoard function
    Moved variables to top

--- chess_validator.py
+++ chess_validator.py
@@ -10,43 +10,66 @@
     #How many attackers and defenders are there on each piece
     #Identify checkmate
 
-StartPos = input("Enter D for default starting position, or board configuration in FEN notation")
-if fen = 'D':
-    StartPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-i = 0    #Position in StartPos array
+#IMPORT STATEMENTS
+
+#VARIABLES (GLOBAL)
+TeamWhite = True    #Playing as white? (Boolean)
+i = 0    #Integer index
+j = 0    #Integer index
+k = 0    #Integer index
+fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"    #Board configuration in FEN
+move = 'e4'    #Chess move in algebraic notation
+q = 'Y'    #User input string
+board = []    #Chess board configuration
+pieces = ['K','k','Q','q','R','r','N','n','B','b','P','p']    #Valid chess pieces list
+
+#FUNCTIONS
+#Display Chess Board on Terminal Screen
+def ShowBoard():
+    while i < 64:    #Display board
+        print(board[i:i+8])
+        i += 8
+
+fen = input("Enter D for default starting position, or board configuration in FEN notation")
+if fen == 'D' or fen == 'd':
+    fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"    #Chess starting position
+i = 0    #Position in fen array
 j = 0    #Board position
-pieces = [K,k,Q,q,R,r,N,n,B,b,P,p]    #Valid pieces list
-board = []
 while j < 64:    #Populate board array from FEN string
     if fen[i] == '/':    #Next rank
-        i += 1    #Ignore and go to to next character
-        if i mod 8 != 0:    # '/' character encountered midway through a rank
-            print("Unexpected / in the board configuration")
-            break
-    elif fen[i].isdigit() == True:
-        if fen[i] > 8:    #More than 8 empty squares in a rank isn't posible in normal chess
-            print("Unexpected extra empty squares in the board configuration")
+        if j % 8 != 0:    # '/' character encountered midway through a rank
+            print("Error: unexpected / in the board configuration: ")
             break
         else:
-            k = 0
-            while k < fen[i]:
-                board[j] = ' '
+            i += 1    #Ignore and go to to next character
+    elif fen[i].isdigit() == True:
+        if int(fen[i]) > 8:    #More than 8 empty squares in a rank isn't posible in normal chess
+            print("Error: extra empty squares in the board configuration")
+            break
+        else:
+            k = 0    #Number of empty squares added to rank
+            while k < int(fen[i]):    #Add specified number of empty squares to board array
+                board.append(' ')
                 k += 1
                 j += 1
-    elif fen[i] in pieces:
-        board[j] = fen[i]
+            i += 1    #Go to next character after adding empty squares
+    elif fen[i] in pieces:    #Checks if character indicates valid chess piece
+        board.append(fen[i])
         i += 1
         j += 1
 #Validate board setup
     #if not 1 king each, throw an error (each color should have exactly 1 King)
     #if not 64 squares accounted for, throw an error (invalid configuration)
     #if more than 8 pawns on either side, throw error (too many pawns)
-q = input ("Are you playing White (W) or Black (B)?")
+i = 0   #Board position
+ShowBoard()
+q = input ("Are you playing White (W) or Black (B)? ")
 if q == 'W' or q == 'w':
     TeamWhite = True
+    move = input("White to move (in algebraic notation): ")
 elif q == 'B' or q == 'b':
-    TeamWhite == False
-move = input("Enter desired move in algebraic notation")
+    TeamWhite = False
+    move = input("Black to move (in algebraic notation): ")
 #validate algebraic notation
     #parse here...
 #get first character of move, and validate piece exists:

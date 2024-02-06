7bb82b002382e5aa1465a6c91b7fdea61ab50a74
Caleb <110494428+griswoct@users.noreply.github.com>
2024-02-06 09:53:35 -0600

    Added ValidCapture Function chess_validator.py
    Added function to check if piece on the square indicated in the move is legally capturable.

--- chess_validator.py
+++ chess_validator.py
@@ -2,13 +2,12 @@
 #PURPOSE: ACCEPT BOARD CONFIGURATION AND CHESS MOVE, VERIFY IF IT IS A LEGAL MOVE
 #LICENSE: THE UNLICENSE
 #AUTHOR: CALEB GRISWOLD
-#UPDATED: 2024-02-05
+#UPDATED: 2024-02-06
 #
 #accepts algebraic notation to move pieces
 #validates of a piece can:
     #move in that way
     #no piece is in the way
-    #not capturing your own piece or opponents King
     #dosen't put your King in check
 #ideas:
     #Heat map of which squares on the boad are controlled by which player
@@ -33,6 +32,8 @@ move = 'e4'    #Chess move in algebraic notation
 square = 'e4'    #Selected square on the board (algebraic notation)
 board = []    #Chess board configuration
 pieces = ['K','k','Q','q','R','r','N','n','B','b','P','p']    #Valid chess pieces list
+xblack = ['q','r','b','n','p']    #Capturable black pieces (no King)
+xwhite = ['Q','R','N','B','P']    #Capturable white pieces (no King)
 
 #FUNCTIONS
 #Display Chess Board on Terminal Screen
@@ -44,31 +45,68 @@ def ShowBoard():
 
 #Convert Algebraic Notated Square to Board Index Number
 def BoardIndex(sq)
-if len(sq) != 2:    #Board location should be 2 characters in algebraic notation
-    return 64    #Invalid index indicating error
-else:
-    if sq[0] == 'a' or sq[0] == 'A':
-        i = (8 - int(sq[1])) * 8
-    elif sq[0] == 'b' or sq[0] == 'B':
-        i = (8 - int(sq[1])) * 8 + 1
-    elif sq[0] == 'c' or sq[0] == 'C':
-        i = (8 - int(sq[1])) * 8 + 2
-    elif sq[0] == 'd' or sq[0] == 'D':
-        i = (8 - int(sq[1])) * 8 + 3
-    elif sq[0] == 'e' or sq[0] == 'E':
-        i = (8 - int(sq[1])) * 8 + 4
-    elif sq[0] == 'f' or sq[0] == 'F':
-        i = (8 - int(sq[1])) * 8 + 5
-    elif sq[0] == 'g' or sq[0] == 'G':
-        i = (8 - int(sq[1])) * 8 + 6
-    elif sq[0] == 'h' or sq[0] == 'H':
-        i = (8 - int(sq[1])) * 8 + 7
-    else:
-        i = 64
-    if i < 0 or i > 63:
+    if len(sq) != 2:    #Board location should be 2 characters in algebraic notation
         return 64    #Invalid index indicating error
     else:
-        return i
+        if sq[0] == 'a' or sq[0] == 'A':
+            i = (8 - int(sq[1])) * 8
+        elif sq[0] == 'b' or sq[0] == 'B':
+            i = (8 - int(sq[1])) * 8 + 1
+        elif sq[0] == 'c' or sq[0] == 'C':
+            i = (8 - int(sq[1])) * 8 + 2
+        elif sq[0] == 'd' or sq[0] == 'D':
+            i = (8 - int(sq[1])) * 8 + 3
+        elif sq[0] == 'e' or sq[0] == 'E':
+            i = (8 - int(sq[1])) * 8 + 4
+        elif sq[0] == 'f' or sq[0] == 'F':
+            i = (8 - int(sq[1])) * 8 + 5
+        elif sq[0] == 'g' or sq[0] == 'G':
+            i = (8 - int(sq[1])) * 8 + 6
+        elif sq[0] == 'h' or sq[0] == 'H':
+            i = (8 - int(sq[1])) * 8 + 7
+        else:
+            i = 64
+        if i < 0 or i > 63:
+            return 64    #Invalid index indicating error
+        else:
+            return i
+
+#Verify the Piece "p" Can Be Captured
+def ValidCapture(p):
+    if white:
+        if p in xblack:
+            if not capture:
+                if check:
+                    move = #Add 'x' at move[-4]
+                else:
+                    move = #Add 'x' at move[-3]
+                print("Changing move to: ", move)
+            return True
+        else:    #A non-capturable piece is already on the square
+            if p == 'k':
+                print("You cannot capture the King")
+            elif p in xwhite or p == 'K':
+                print("Your ", p, " is already on ", square)
+            else:
+                print("Something went wrong. ", p, " is on ", square)
+            return False
+    else:
+        if p in xwhite:
+            if not capture:
+                if check:
+                    move = #Add 'x' at move[-4]
+                else:
+                    move = #Add 'x' at move[-3]
+                print("Changing move to: ", move)
+            return True
+        else:    #A non-capturable piece is already on the square
+            if p == 'K':
+                print("You cannot capture the King")
+            elif p in xblack or p == 'k':
+                print("Your ", p, " is already on ", square)
+            else:
+                print("Something went wrong. ", p, " is on ", square)
+            return False
 
 #MAIN BODY
 #Get Chess Board Setup
@@ -102,7 +140,7 @@ while j < 64:    #Populate board array from FEN string
         i += 1
         j += 1
 #Validate Chess Board Setup
-if len.(board) != 64:
+if len(board) != 64:
     print("Error: incomplete board configuration")    #Board array should have 64 elements
     ShowBoard()
 for i in pieces:
@@ -145,7 +183,7 @@ elif move[-1] == '0' or move[-1] == 'o' or move[-1] == 'O':    #Castle
             square = 'd8'
 else:
     square = move[-2:]
-if move[1] == 'x':
+if move[1] == 'x' or move[1] == 'X':
     capture = True
 if move[0] == 'K' or move[0] == 'k' or move[0] == '0' or move[0] == 'O' or move[0] == 'o':
     piece = 'K'
@@ -162,10 +200,16 @@ elif move[0] == 'a' or move[0] == 'b' or move[0] == 'c' or move[0] == 'd' or mov
 else:
     print("Error: could not parse move notation: ", move)
 #Validate Selected Square
-b = BoardIndex(square)
+b = int(BoardIndex(square))
 if b == 64:    #Indicates error
     print("Error: ", square, " not found on board")
-#Check if square is occupied, validate capture
+#Validate Capture
+if board[b] != ' ':
+    if ValidCapture(board[b]):
+        capture = True
+    else:
+        print("Error: invalid capture")
+
 #count number of that piece:
     #if first character is K or o, skip counting pieces
         #count = 1

b00203107246d27dbb53f4e3f7fa3d5bc40ec8a1
Caleb <110494428+griswoct@users.noreply.github.com>
2024-05-16 08:27:54 -0500

    Added Recording Indicator to ExcelStocks.vb
    Added formatting function to turn the button text green when recording, and red when recording has stopped

--- ExcelStocks.vb
+++ ExcelStocks.vb
@@ -1,23 +1,49 @@
+'EXCEL STOCKS
+'PURPOSE: RECORD STOCK PRICES FROM EXCEL
+'LICENSE: THE UNLICENSE
+'AUTHOR: CALEB GRISWOLD
+'UPDATED: 2024-05-16
+
 Option Explicit
 
 Sub Rec()
     Dim i As Integer
     Dim OpenBell, EoB As Date
     Dim Time0, Time1
-    OpenBell = TimeValue("08:30:00AM ")
+    OpenBell = TimeValue("08:30:30AM ")
     EoB = TimeValue("03:00:59PM")
     Workbooks("TickTrack.xlsm").Activate
+    Sheet2.Activate
         If Time < OpenBell Then 'Market closed
             Application.OnTime OpenBell, "TickTrack.xlsm!Sheet2.Rec"   'Try again at the opening bell
+            Call Running
             MsgBox "Market not yet open"
         Else
             If Time > EoB Then  'Market closed
+                Call Stopped
                 MsgBox "Closed for the day"
             Else
-                Application.OnTime Now + TimeValue("00:09:59"), "TickTrack.xlsm!Sheet2.Rec"
+                Application.OnTime Now + TimeValue("00:10:00"), "TickTrack.xlsm!Sheet2.Rec"
+                Call Running
                 Call SaveData
             End If
         End If
+End Sub
+
+Sub Stopped()
+    ActiveSheet.Shapes.Range(Array("Button 1")).Select
+    With Selection.Font
+        .FontStyle = "Bold"
+        .ColorIndex = 3
+    End With
+End Sub
+
+Sub Running()
+    ActiveSheet.Shapes.Range(Array("Button 1")).Select
+    With Selection.Font
+        .FontStyle = "Italic"
+        .ColorIndex = 4
+    End With
 End Sub
 
 Sub SaveData()

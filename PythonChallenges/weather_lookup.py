e047f4e38d73d0929b1625f7c43d925ba693c7d6
Caleb <110494428+griswoct@users.noreply.github.com>
2025-09-24 15:28:54 -0500

    Create weather_lookup.py

---
+++ PythonChallenges/weather_lookup.py
@@ -0,0 +1,9 @@
+'''
+#WEATHER LOOKUP
+PURPOSE: LOOK UP WEATHER CONDITIONS VIA PYTHON SCRIPT
+LICENSE: THE UNLICENSE
+AUTHOR: CALEB GRISWOLD
+CREATED: 09/24/2025
+'''
+#Use NOAA api: https://api.weather.gov/
+#Need to figure out how to get location and pass to API

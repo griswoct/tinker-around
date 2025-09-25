5920b19f1569dd75a3629c31be08cf4815529e91
Caleb <110494428+griswoct@users.noreply.github.com>
2025-09-25 15:24:26 -0500

    Add GET Outline to weather_lookup.py

--- PythonChallenges/weather_lookup.py
+++ PythonChallenges/weather_lookup.py
@@ -7,3 +7,10 @@ CREATED: 09/24/2025
 '''
 #Use NOAA api: https://api.weather.gov/
 #Need to figure out how to get location and pass to API
+import requests
+
+base_url = "https://api.weather.gov/"
+location = ""  #Station ID goes here
+type = "/stations/" + location + "/observations/latest"
+
+response = requests.get(url)

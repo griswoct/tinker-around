5b17978a331ada4c81b2c89afe8ab2d44b6a68b0
Caleb <110494428+griswoct@users.noreply.github.com>
2025-09-26 14:31:36 -0500

    Adding Location Lookup API to weather_lookup.py
    https://weather-gov.github.io/api/general-faqs

--- PythonChallenges/weather_lookup.py
+++ PythonChallenges/weather_lookup.py
@@ -1,16 +1,20 @@
 '''
 #WEATHER LOOKUP
-PURPOSE: LOOK UP WEATHER CONDITIONS VIA PYTHON SCRIPT
-LICENSE: THE UNLICENSE
-AUTHOR: CALEB GRISWOLD
-CREATED: 09/24/2025
+#PURPOSE: LOOK UP WEATHER CONDITIONS VIA PYTHON SCRIPT
+#LICENSE: THE UNLICENSE
+#AUTHOR: CALEB GRISWOLD
+#CREATED: 09/24/2025
 '''
 #Use NOAA api: https://api.weather.gov/
 #Need to figure out how to get location and pass to API
 import requests
 
-base_url = "https://api.weather.gov/"
-location = ""  #Station ID goes here
+api_url = "https://api.weather.gov/"
+coordinates = input("Where are you located? (latitude,longitude): ")
+print("Coordinates: ",coordinates)  #for testing
+location = requests.get("https://api.weather.gov/points/"+coordinates)
+print("Location: ",location)
 type = "/stations/" + location + "/observations/latest"
+url = api_url+location
 
-response = requests.get(url)
+response = requests.get(url+type)

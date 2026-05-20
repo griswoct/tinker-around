b6b12de9a5915913637a8c84860861de0533dcdf
Caleb <110494428+griswoct@users.noreply.github.com>
2026-05-20 15:27:24 -0500

    Added Loops for Ship Placement in battleship.py

--- battleship.py
+++ battleship.py
@@ -20,13 +20,17 @@
 * Parallel (all vertical or all horizontal)
 '''
 
+#Import
+import random
+
 #initialize variables
 sea1 = [None] * 100
 screen1 = [None] * 100
 sea2 = [None] * 100
 screen2 = [None] * 100
-remaining_ships1 = ['P','S','M','B','C']
-remaining_ships2 = ['P','S','M','B','C']
+ships = ['P','S','M','B','C']
+remaining_ships1 = ships
+remaining_ships2 = ships
 boat_strategy1 = 0
 dart_strategy1 = 0
 boat_strategy2 = 0
@@ -35,20 +39,30 @@ padded = 'Z'
 edges = 'Z'
 
 def place_boats(padded, edges):
-    while remaining_ships1 is not None: #while remaining_ships is not None:
-        #get the last element of remaining ships
-        #choose a random number x from 1-100
-        #if sea1(x) is open:
-            #choose random number direction from 1 to 4
-            #while less than the length of the boat:
-                #increment that direction
-                #record locations
-                #if off the board or occupied spot
-                    #reset locations, boat length progress, direction, and starting point x
-                    #try placing again
-            #record value of ramaining_ships1[-1] to sea1[locations]
-        #remove last element of remaining ships
-    return sea1
+    sea = [0] * 100
+    x = random.randint(0,99)
+    n = 0
+    while n < len(ships):
+        boat = ships[n]
+        while sea[x] is not 0:  #find an unoccupied part of the sea
+            x = random.randint(0,99)    #choose a random number x from 0-99
+        y = random.randint(1,4)    #choose random number direction from 1 to 4
+        z = 0
+        #choose size based on ship type
+        #check if the boat will go off the end of the board
+            #if up, x - 10 * (size - 1) >= 0
+            #if down, x + 10 * (size - 1) <= 99
+            #if left, x - (size - 1) >= 0
+            #if right, x + (size - 1) <= 99
+        #while less than the length of the boat:
+            #increment that direction
+            #record locations
+            #if occupied spot
+                #reset locations, boat length progress, direction, and starting point x
+                #try placing again
+        #record value of ramaining_ships1[-1] to sea1[locations]
+        #remove last element of remaining ship
+    return sea
 
 boat1 = input("Choose ship placement strategy:\n1. Random\n2. Spaced\n3. Clumped\n4. Touch Edge\n5. Avoid Edge\n6.Touch Edge Spaced\n7. Touch Edge Clumped\n8.Avoid Edge Spaced\n9. Avoid Edge Clumped\n")
 while boat1 not in [1,2,3,4,5,6,7,8,9]:
@@ -61,7 +75,7 @@ if boat1 in [4,6,7]:
     edges = 'T'
 elif boat1 in [5,8,9]:
     edges = 'F'
-sea1 = place_boats(padded,edges)
+sea1 = place_boats(padded, edges)
 
 while remaining_ships1 is not None: #while remaining_ships is not None:    
 
